a7d78504f8df798f490fcb757243fbac8d78dfac
jumaTech <86480489+jumatech78@users.noreply.github.com>
2022-10-04 13:58:24 +0300

    created contact me page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,33 +2,39 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div className="pb-8">
+        <div>
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             About
           </p>
         </div>
+
         <p className="text-xl mt-20">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit omnis
-          quasi ratione impedit excepturi nisi assumenda nam sapiente facere
-          vel, nemo, perferendis tenetur ipsum quos iure commodi nulla
-          reiciendis veritatis laboriosam dolorum. Praesentium adipisci labore
-          nemo distinctio voluptatum? Ad animi quia delectus, sequi dolore
-          labore? Praesentium quisquam eaque iure? Quas!
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius,
+          deserunt illum mollitia officiis qui exercitationem perferendis neque
+          quasi a recusandae necessitatibus tempora iusto! Blanditiis error
+          iste, totam fugiat recusandae rerum laborum perferendis molestiae
+          aperiam asperiores nemo. Magni dolor maxime debitis vitae, eaque hic
+          ab mollitia voluptatibus, a nostrum eveniet laborum!
         </p>
+
         <br />
+
         <p className="text-xl">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores
-          provident illo vero molestiae. Dolorem ipsam quod provident officiis
-          at, quibusdam soluta ex fugit veritatis ut, ullam voluptates odit
-          voluptatum repellendus placeat quam dignissimos natus animi. Cum
-          molestiae suscipit perspiciatis. Iure, sit cumque. Magnam
-          exercitationem natus assumenda fugit eos perferendis blanditiis?
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum
+          pariatur, vel similique sint, nobis aspernatur ut praesentium
+          explicabo ipsam aliquid quasi laboriosam et culpa possimus repudiandae
+          quisquam ullam maiores ab unde. Fugiat odio mollitia nemo alias.
+          Commodi facilis atque nulla vero voluptatem explicabo. Quibusdam,
+          magni quo! Eum cupiditate debitis labore.
         </p>
       </div>
     </div>
-  )
+  );
 };
 
 export default About;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { MdKeyboardArrowRight } from "react-icons/md";
-import heroImage from "../assets/heroImage.JPG";
+import HeroImage from "../assets/heroImage.JPG";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { Link } from "react-scroll";
 
 const Home = () => {
   return (
@@ -11,33 +12,39 @@ const Home = () => {
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            I'm a Front End Developer
+            I'm a Full Stack Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
             I have 2 years of experience building and designing software.
-            Currently, I love to work on both web and mobile application using
-            technologies like React.js, React-Native, Next.js and Firebase.
+            Currently, I love to work on both mobile and web application using technologies like
+            React, React-Native, Tailwind, Next JS, Firebase and Django.
           </p>
+
           <div>
-            <button className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
+            <Link
+              to="portfolio"
+              smooth
+              duration={500}
+              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+            >
               Portfolio
               <span className="group-hover:rotate-90 duration-300">
-                <MdKeyboardArrowRight size={25} className="ml-1" />
+                <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
               </span>
-            </button>
+            </Link>
           </div>
         </div>
 
-        <div className="">
+        <div>
           <img
-            src={heroImage}
-            alt="profile"
-            className="rounded-2xl mx-auto w-2/3 md:w-full" />
-           
+            src={HeroImage}
+            alt="my profile"
+            className="rounded-2xl mx-auto w-2/3 md:w-full"
+          />
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default Home;
