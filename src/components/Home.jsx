8bf6043cdd24826ec95a9f20039000ecc0b515d0
jumaTech <86480489+jumatech78@users.noreply.github.com>
2022-10-04 12:13:38 +0300

    created experience page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
-          <h2 className="text-4xl sm:text-6xl font-bold text-white">
+          <h2 className="text-4xl sm:text-7xl font-bold text-white">
             I'm a Front End Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
@@ -22,7 +22,7 @@ const Home = () => {
             <button className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
               Portfolio
               <span className="group-hover:rotate-90 duration-300">
-                <MdKeyboardArrowRight size={25} />
+                <MdKeyboardArrowRight size={25} className="ml-1" />
               </span>
             </button>
           </div>
@@ -32,12 +32,12 @@ const Home = () => {
           <img
             src={heroImage}
             alt="profile"
-            className="rounded-full mx-auto w-2/3 md:w-full"
-          />
+            className="rounded-2xl mx-auto w-2/3 md:w-full" />
+           
         </div>
       </div>
     </div>
-  );
+  )
 };
 
 export default Home;
