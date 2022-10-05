3e580cc6597488e1958d922a488206af54fd80f2
jumaTech <86480489+jumatech78@users.noreply.github.com>
2022-10-05 21:46:53 +0300

    updates on contact us

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
 
         <div className=" flex justify-center items-center">
           <form
-            action="https://getform.io/f/61c99527-2b15-42cf-9b55-ad37d2f7daa6"
+            action="https://getform.io/f/354e7d14-b4a2-4d50-b9ae-846ce51ad3cc"
             method="POST"
             className=" flex flex-col w-full md:w-1/2"
           >
